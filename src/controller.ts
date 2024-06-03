8ede632bb1615db687d773d35c5ab9b4cadc0d7e
denyncrawford <kiquerangelb@gmail.com>
2024-06-02 21:26:51 -0300

    feat: remove react types from tsx

--- src/controller.ts
+++ src/controller.ts
@@ -1,14 +1,14 @@
-// deno-lint-ignore-file verbatim-module-syntax
+// deno-lint-ignore-file no-explicit-any
 /**
  * This module contains the controller component
  * A react component that allows you to create search filters for your react application, using a simple api and input driven by default.
  * @module
  */
 
-// @deno-types="npm:@types/react@^18.0.0"
-import { FormEvent, MutableRefObject } from "react";
 
 import type { IRegisterProps, IControl } from "./types.ts";
+import { Fragment as _Fragment, jsx as _jsx } from "react/jsx-runtime";
+
 
 /**
  * Pops for the controller field
@@ -24,29 +24,24 @@ export interface IControllerFieldPops {
  * Renders the controller field, this is util when you have a custom input that needs to be registered
  * @param props - Renders the controller field
  */
-export const Controller = (
-  props: {
-    control: MutableRefObject<IControl>;
-    render: (field: IControllerFieldPops) => JSX.Element;
-  } & IRegisterProps
-): JSX.Element => {
+export const Controller = (props: {
+  control: { current: IControl };
+  render: (field: IControllerFieldPops) => JSX.Element;
+} & IRegisterProps) => {
   const found = props.control.current.register(props);
-
-  return (
-    <>
-      {found &&
-        props.render({
-          onChange: (value: unknown) => {
-            found.onChange?.({
-              target: {
-                value,
-                checked: !!value,
-              },
-            } as unknown as FormEvent<HTMLInputElement>);
-          },
-          value: found.value,
-          checked: !!found.checked,
-        })}
-    </>
-  );
+  return /*#__PURE__*/_jsx(_Fragment, {
+    children: found && props.render({
+      onChange: value => {
+        let _found$onChange;
+        (_found$onChange = found.onChange) === null || _found$onChange === void 0 || _found$onChange.call(found, {
+          target: {
+            value,
+            checked: !!value
+          }
+        } as any);
+      },
+      value: found.value,
+      checked: !!found.checked
+    })
+  });
 };
